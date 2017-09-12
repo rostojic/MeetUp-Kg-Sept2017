0d945de19cb6f223bc4ce3cfc357452ce5836158
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2017-09-12 16:11:52 +0200

    Final version - get SP lists
    OK

--- hello-cloud-sharepoint/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ hello-cloud-sharepoint/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -1,3 +1,12 @@
+import {
+  Environment,
+  EnvironmentType
+} from '@microsoft/sp-core-library';
+import {
+  SPHttpClient,
+  SPHttpClientResponse   
+} from '@microsoft/sp-http';
+
 import { Version } from '@microsoft/sp-core-library';
 import {
   BaseClientSideWebPart,
@@ -12,6 +21,16 @@ import { escape } from '@microsoft/sp-lodash-subset';
 import styles from './HelloWorldWebPart.module.scss';
 import * as strings from 'HelloWorldWebPartStrings';
 import { IHelloWorldWebPartProps } from './IHelloWorldWebPartProps';
+import MockHttpClient from './MockHttpClient';
+
+export interface ISPLists {
+  value: ISPList[];
+}
+
+export interface ISPList {
+  Title: string;
+  Id: string;
+}
 
 export default class HelloWorldWebPartWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
 
@@ -24,15 +43,62 @@ export default class HelloWorldWebPartWebPart extends BaseClientSideWebPart<IHel
             <span class="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
             <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
             <p class="ms-font-l ms-fontColor-white">${escape(this.properties.description)}</p>
-            <p class="ms-font-l ms-fontColor-white">${escape(this.properties.test2)}</p>
             <p class="ms-font-l ms-fontColor-white">Loading from ${escape(this.context.pageContext.web.title)}</p>
             <a href="https://aka.ms/spfx" class="${styles.button}">
               <span class="${styles.label}">Learn more</span>
             </a>
           </div>
         </div>
-      </div>
+      </div>  
+      <div id="spListContainer" />
     </div>`;
+    this._renderListAsync(); 
+  }
+
+  private _renderList(items: ISPList[]): void {
+    let html: string = '';
+    items.forEach((item: ISPList) => {
+      html += `
+        <ul class="${styles.list}">
+            <li class="${styles.listItem}">
+                <span class="ms-font-l">${item.Title}</span>
+            </li>
+        </ul>`;
+    });
+
+    const listContainer: Element = this.domElement.querySelector('#spListContainer');
+    listContainer.innerHTML = html;
+  }
+
+  private _renderListAsync(): void {
+    // Local environment
+    if (Environment.type === EnvironmentType.Local) {
+      this._getMockListData().then((response) => {
+        this._renderList(response.value);
+      });
+    }
+    else if (Environment.type == EnvironmentType.SharePoint || 
+              Environment.type == EnvironmentType.ClassicSharePoint) {
+      this._getListData()
+        .then((response) => {
+          this._renderList(response.value);
+        });
+    }
+  }
+
+  private _getListData(): Promise<ISPLists> {
+    return this.context.spHttpClient.get(this.context.pageContext.web.absoluteUrl + `/_api/web/lists?$filter=Hidden eq false`, SPHttpClient.configurations.v1)
+      .then((response: SPHttpClientResponse) => {
+        return response.json();
+      });
+  }
+
+  private _getMockListData(): Promise<ISPLists> {
+    return MockHttpClient.get()
+      .then((data: ISPList[]) => {
+        var listData: ISPLists = { value: data };
+        return listData;
+      }) as Promise<ISPLists>;
   }
 
   protected get dataVersion(): Version {
